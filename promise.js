6dc25b3f163ae4a742d07543d6b188ebfeafef60
Lyiran <lyiyi0834@163.com>
2019-03-18 00:00:21 +0800

    promise-theory

---
+++ promise.js
@@ -0,0 +1,63 @@
+function Promise(executor) {
+  this.status = 'pending';
+  this.value = undefined;
+  this.reason = undefined;
+  let self = this;
+  self.onResolveCallbacks = [];
+  self.onRejectedCallbacks = [];
+  function resolve(value) {
+    if(self.status === 'pending') {
+      self.value = value;
+      self.status = 'fulfilled';
+      self.onResolveCallbacks.forEach(fn => fn());
+    }
+  }
+
+  function reject() {
+    if(self.status === 'pending') {
+      self.reason = reason;
+      self.status = 'rejected';
+      self.onRejectedCallbacks.forEach(fn => fn());
+    }
+  }
+
+  try {
+    executor(resolve, reject);
+  }catch(e) {
+    console.log(e);
+    reject(e);
+  }
+}
+
+// 实现promise.then方法每次返回的都是一个新的promise，不能返回本身
+// promise2就是当前then返回的promise
+// x就是当前then中成功或者失败回调的返回结果
+
+// 因为此方法 可能混合着别人的逻辑 所以尽可能考虑周全
+function resolvePromise(promise2, x, resolve, reject) {
+  // 对x进行判断 如果x是一个普通值 则直接resovle就可以了
+  // 如果x是一个promise 采用promise的状态
+  if(promise2 === x ) {
+    return reject(new TypeError('循环引用'));
+  }
+  // 这种情况下x有可能是一个promise
+  if(x !== null && (typeof x === 'object' || typeof x === 'function')) {
+    try{
+      let then = x.then; // 看当前的promise有没有then方法 有可能取then的时候报错
+      if (typeof then === 'function') { // 是一个promise
+        then.call(x, y => {
+          resolve(y);
+        }, r => {
+          reject(r);
+        }); //用刚才取出的then方法，不要再去取值了 如果再取值可能会发生异常
+
+      } else { // {then: {}}
+        resolve (x);
+      }
+    } catch(e) {
+      reject(e);
+    }
+  } else {
+    resolve (x);
+  }
+}
